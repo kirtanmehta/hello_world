298242517e83eb8a96dced674062fc6056e9548b
kirtanmehta <kirtanmehta1296@gmail.com>
2017-10-14 17:24:26 -0700

    brach_edit

--- adder.v
+++ adder.v
@@ -2,8 +2,11 @@ module adder
 (input a ,g,
 output sum, carry);
 
-assign sum = a xor b;
+assign sum = a ^ b;
 assign carry = a | b;
 endmodule
 
 // now i made some changes
+
+
+// the edit is now made by kirtan_test_branch
